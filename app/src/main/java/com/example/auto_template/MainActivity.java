be9116d22265bbe609a887aae4fb0990b9e79349
kyusieun <kyusieun@gmail.com>
2023-11-28 17:17:04 +0900

    firestore 데이터 추가 함수

--- app/src/main/java/com/example/auto_template/MainActivity.java
+++ app/src/main/java/com/example/auto_template/MainActivity.java
@@ -16,11 +16,15 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.auto_template.databinding.ActivityMainBinding;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.search.SearchView;
+import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
@@ -29,6 +33,10 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.time.temporal.TemporalAccessor;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
 
@@ -83,27 +91,79 @@ public class MainActivity extends AppCompatActivity {
             binding.searchView.setVisibility(View.VISIBLE);
         });
 
-        Log.d("LOGIN3", loadJsonFromLocal());
+//        Log.d("LOGIN3", loadJsonFromLocal());
         // Firestore에서 data loding 후 LogCat에 출력
         // document.getData()를 사용해서 변수에 저장 가능
         // 현재 document.getData() 쿼리의 결과는 아래 주석과 같음
         // 	{latest_use: 2023년 11월 27일 오전 5시 5분 44초 UTC+9, content: "내용", id: 0, reference: 4, tag: ["tag1", "tag2"], last_edit: 2023년 11월 26일 오전 5시 5분 28초 UTC+9, title: "안녕하세요"}
+
+
+        // FireStore 데이터 추가 함수입니다.
+//        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+        // 원래 currentUser != null 인데 비활성화 하려고 null 넣었습니다.
+        if (null != null) {
+//            String userUid = currentUser.getUid();
+            String userUid = "user1";
+
+            Map<String, Object> userData = new HashMap<>();
+            Map<String, Object> templateData = new HashMap<>();
+            templateData.put("last_edit", new Timestamp(new Date()));
+            templateData.put("latest_use", new Timestamp(new Date()));
+            templateData.put("reference", 4);
+            List<String> tags = Arrays.asList("tag1", "tag2", "tag3");
+            templateData.put("tag", tags);
+            templateData.put("title", "제목");
+            templateData.put("content", "안녕하세요. {{고객명}}님, 구매하신 상품에 대한 추가 정보를 [[날짜]]까지 회신하여 주시면 추가 조치를 해드리겠습니다. 추가 문의사항이 있으시다면 {{주소}}로 방문해주세요.");
+            String template_name = new Timestamp(new Date()).toString();
+            userData.put(template_name, templateData);
+//            userData.put("email", currentUser.getEmail());
+            // 사용자 UID를 기반으로 데이터 추가
+            db.collection("users").document(userUid)
+                    .set(userData, SetOptions.merge())
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Log.d("Firestore", "DocumentSnapshot successfully written!");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(Exception e) {
+                            Log.w("Firestore", "Error writing document", e);
+                        }
+                    });
+        }
+
+
+
         docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
             @Override
             public void onComplete(Task<DocumentSnapshot> task) {
                 if (task.isSuccessful()) {
                     DocumentSnapshot document = task.getResult();
                     if (document.exists()) {
-                        Log.d("LOGIN", "DocumentSnapshot data: " + document.getData().toString());
-                        Log.d("LOGIN2", document.getData().get("template").getClass().toString());
-                        Log.d("LOGINB", items.toString());
+                        // "template" 필드를 Map으로 가져옴
+                        Map<String, Object> templateMap = (Map<String, Object>) document.get("template");
+
+                        if (templateMap != null) {
+                            // "content" 필드를 가져옴
+                            String content = (String) templateMap.get("content");
+                            Log.d("FireStore", "content: " + content);
+                            // content를 사용하여 원하는 작업 수행
+                            // 예: TextView에 content를 설정
+                            // textView.setText(content);
+                        } else {
+                            // "template" 필드가 없는 경우 처리
+                        }
+                        Log.d("FireStore", "DocumentSnapshot data: " + document.getData().toString());
+
                         items = parseMapToTemplateCollection(document.getData());
-                        Log.d("LOGINA", items.toString());
                     } else {
-                        Log.d("LOGIN", "No such document");
+                        Log.d("FireStore", "No such document");
                     }
                 } else {
-                    Log.d("LOGIN", "get failed with ", task.getException());
+                    Log.d("FireStore", "get failed with ", task.getException());
                 }
                 myAdapter.addItems(items);
                 binding.recyclerView.setAdapter(myAdapter);
